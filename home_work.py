e26cd547e57acfa0b41ae6e1bd1195bc377a004a
Sergey <sergey.a.kulackov@yandex.ru>
2026-01-16 11:27:20 +0300

    Отправка запроса с методом Get, с использованием ООП

--- home_work.py
+++ home_work.py
@@ -1,13 +1,50 @@
 import requests
 
 
-url = "https://api.chucknorris.io/jokes/random" #Запрашиваемый url
-print(url)
+class TestCreateJoke:
+    def __init__(self):
+        self.types = None
+        self.number_of_type = None
 
-response = requests.get(url) #Get запрос (запрос шутки)
-print("Статус код: " + str(response.status_code)) #Вывод статуса ответа
+    def get_categories(self):
+        print("123")
+        self.categories = requests.get("https://api.chucknorris.io/jokes/categories").json() #Получение категорий
+        print("Категории получены")
+        return self.categories
 
-assert 200 == response.status_code, "Статус код не 200" #Проверка статуса ответа
-print("Статус код верен")
+    def choose_category(self):
+        categories = self.get_categories()
+        print(f"Введите число от 0 до 15, соответствующее выбранной категории") #Выбор категории
+        for index, category in enumerate(categories):
+            print(f"{index} - {category}")
+        while True:
+            try:
+                self.number_of_type = int(input())
+                if 0 <= self.number_of_type <= 15:
+                    print(f"Выбрана категория {categories[self.number_of_type]}")
+                    self.types = categories[self.number_of_type]    #выбранная категория
+                    return self.types
+                else:
+                    print("Введите число от 0 до 15")
+            except ValueError:
+                print("Введено не чиcло")
 
-print(response.json()) #Вывод ответа
+    def get_joke(self):
+        types = self.choose_category()
+        response = requests.get(f"https://api.chucknorris.io/jokes/random?category={types}")  # Get запрос (запрос шутки по категории)
+        print(response.json())
+        return response
+
+
+if __name__ == "__main__":
+    joke_tester = TestCreateJoke()
+    response = joke_tester.get_joke()
+
+    assert response.status_code == 200, "Статус код не 200" #Проверка статуса ответа
+    print("Статус код верен")
+
+    assert response.json()["categories"][0] == joke_tester.types, "Выбранная категория не совпадает"  # Проверка категории шутки
+    print("Категория шутки верная")
+
+    assert "Chuck" in response.json()["value"], "Шутка не содержит Chuck"  # Проверка категории шутки
+    print(response.json()["value"])
