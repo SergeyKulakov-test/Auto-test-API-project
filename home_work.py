96c906dd748e3fe5a5a79f865d56526fd70ca93f
Sergey <sergey.a.kulackov@yandex.ru>
2026-01-19 09:35:45 +0300

    Отправка запроса методом Put_1

--- home_work.py
+++ home_work.py
@@ -1,13 +1,14 @@
 import requests
 
 
-class TestMapApi:
+class MapApi:
     def __init__(self):
         self.url = "https://rahulshettyacademy.com" #Базовая ссылка
         self.post_resource = "/maps/api/place/add/json" #Чать для POST запроса
         self.get_resource = "/maps/api/place/get/json" #Чать для GET запроса
+        self.put_resource = "/maps/api/place/update/json"  #Чать для PUT запроса
         self.key = "?key=qaclick123"                    #Ключ
-        #Тело запроса
+        #Тело POST запроса
         self.body = {"location": {
                 "lat": -38.383494,
                 "lng": 33.427362
@@ -18,14 +19,40 @@ class TestMapApi:
                 "address": "29, side layout, cohen 09",
                 "types": ["shoe park", "shop"],
                 "website": "http://google.com",
-                "language": "French-IN"}
-
+                "language": "French-IN"
+                }
+        # Тело PUT запроса
+        self.put_body = {
+            "place_id": "",
+            "address": "",
+            "language": "RU_ru",
+            "key": "qaclick123"
+        }
+    #POST запрос
     # Создание локации
     def create_location(self, body, post_url):
         request_post = requests.post(post_url, json=body)
         print(request_post.json())
         return request_post
 
+    # Get запрос
+    def get_request(self, get_url):
+        request_get = requests.get(get_url)
+        print(request_get)
+        return request_get
+
+    # PUT запрос
+    def put_request(self, body, url):
+        request_put = requests.put(url, json=body)
+        return request_put
+
+    # Создание тела PUT запроса
+    def create_body_put_request(self, place_id, address):
+        self.put_body['place_id'] = place_id
+        self.put_body['address'] = address
+        return self.put_body
+
+    # Работа с фалом
     # Сохранение в файл ID
     def save_place_id(self, request_post):
         with open('test_place_id.txt', 'a', encoding='utf-8') as file:
@@ -42,19 +69,12 @@ class TestMapApi:
                 file.writelines(lines)
         print("ID записан в файл")
 
-    # Get запрос
-    def get_request(self, get_url):
-        request_get = requests.get(get_url)
-        print(request_get)
-        return request_get
-
     # Получение ID из файла
     def get_place_id_in_file(self):
         with open('test_place_id.txt', 'r', encoding='utf-8') as file:
             lines = file.readlines()
         lines = [s.strip("\n") for s in lines]
         return lines
-
 
 if __name__ == "__main__":
     create_location = TestMapApi()  # Создаие экземпляра класса
--- test_API_PUT_request.py
+++ test_API_PUT_request.py
@@ -1,16 +1,15 @@
-from API_PUT_request import TestMapApiPutRequest
-from home_work import TestMapApi
+from home_work import MapApi
 
 
 if __name__ == "__main__":
-    test_put_request = TestMapApiPutRequest()
-    new_address = test_put_request.put_body["address"]  # Новый адрес
+    test_put_request = MapApi()
+    new_address = "10 Artelnaya street"  # Новый адрес
     print(new_address)
 
     place_id = test_put_request.get_place_id_in_file()[0]  # Получение ID из файла
     print(place_id)
 
-    body = test_put_request.create_body_put_request(place_id)  # Добавление ID в тело запроса
+    body = test_put_request.create_body_put_request(place_id, new_address)  # Добавление ID в тело запроса
     print(body)
 
     url = test_put_request.url + test_put_request.put_resource + test_put_request.key  # Ссылка для PUT запроса
@@ -23,10 +22,9 @@ if __name__ == "__main__":
     assert result_put.status_code == 200, "Статус код не верен"  # Проверка PUT запроса
     print(f"Статус код PUT запроса: {result_put.status_code}")
 
-    create_location = TestMapApi()
-    get_url = create_location.url + create_location.get_resource + create_location.key + "&place_id=" + place_id  # Ссылка для GET запроса
+    get_url = test_put_request.url + test_put_request.get_resource + test_put_request.key + "&place_id=" + place_id  # Ссылка для GET запроса
 
-    get_result = create_location.get_request(get_url)  # Получение данных для проверки
+    get_result = test_put_request.get_request(get_url)  # Получение данных для проверки
     print(get_result.json())
 
     assert get_result.status_code == 200, "Статус код верен"  # Проверка GET запроса
